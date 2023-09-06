1932e368009cb94215ab73b9e426876481c291e4
unknown <21bogdan09@gmail.com>
2023-09-06 14:34:32 +0300

    commit

--- components/layouts/Background.vue
+++ components/layouts/Background.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="background"
-    class="fixed h-full w-full -z-10"
+    class="fixed left-0 right-0 bottom-0 top-[-70px] pb-[70px] translate-y-[70px] -z-10"
     style="
       background: radial-gradient(ellipse at center, #08192a 19%, black 100%);
     "
