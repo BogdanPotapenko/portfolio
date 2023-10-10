132e62ddd4dbf64b692c852e51e7f3b66abcc436
unknown <21bogdan09@gmail.com>
2023-10-10 12:18:17 +0300

    fix

--- components/layouts/Background.vue
+++ components/layouts/Background.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="background"
-    class="fixed left-0 right-0 bottom-0 -top-[100px] pb-[100px] translate-y-[100px] -z-10"
+    class="min-h-screen fixed left-0 right-0 -top-[100px] pb-[100px] translate-y-[100px] -z-10"
     style="
       background: radial-gradient(ellipse at center, #08192a 10%, black 100%);
     "
